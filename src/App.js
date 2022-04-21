9c201a9e324472c872e9fe4ea9c0fd23adf7de1d
Michelle <tianw0420@gmail.com>
2022-04-20 21:33:29 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/*hierarchy: 
+  App 
+    profile
+    work  
+      personal statement
+      experience
+      education
+      skills
+
+*/
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
